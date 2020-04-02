15349a41903b04be5f673784789e7723aadf26a3
Guilhem BEILLARD <guilhem.beillard@Viks-MacBook-Pro.local>
2020-04-02 19:40:48 +0200

    added some puppeteer code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import account from './account.json'
 
   // login
   await page.goto('https://ts1.travian.fr/login.php');
-  await page.screenshot({path: 'view0.png'});
+  await page.screenshot({ path: 'view0.png' });
   await page.type('[name=name]', account.name);
   await page.type('[name=password]', account.password);
   await Promise.all([
@@ -19,21 +19,22 @@ import account from './account.json'
   // navigate to dorf1
   await page.goto('https://ts1.travian.fr/dorf1.php');
 
-  await page.screenshot({path: 'view1.png'});
+  await page.screenshot({ path: 'view1.png' });
 
   const [wood, clay, iron, food, list] = await Promise.all([
     await page.$eval('#l1', (element) => element.innerText),
     await page.$eval('#l2', (element) => element.innerText),
     await page.$eval('#l3', (element) => element.innerText),
     await page.$eval('#l4', (element) => element.innerText),
-    await page.$eval('#l4', (element) => ({
-      building: 
-    })),
+    await page.$$eval('.buildingList > .boxes-contents > ul]', (elements) => elements.map((element) => ({
+      building: element.children[1].innerText,
+      remainingTime: element.children[2].innerText,
+    }))),
   ]);
 
   console.log('wood: ', wood, ' | clay ', clay, ' | iron ', iron, ' | food ', food);
   // navigate to dorf2
   await page.goto('https://ts1.travian.fr/dorf2.php');
-  await page.screenshot({path: 'view2.png'});
+  await page.screenshot({ path: 'view2.png' });
   await browser.close();
 })();
