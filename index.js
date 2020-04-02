0bce1a9f2d965e2680291f25a976e67193879d9d
Guilhem BEILLARD <guilhem.beillard@Viks-MacBook-Pro.local>
2020-04-02 17:41:45 +0200

    puppeteer install

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto('https://ts1.travian.fr/dorf1.php');
   await page.screenshot({path: 'example.png'});
 
   await browser.close();
